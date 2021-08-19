f032212f257397288460315c7aef5e1b5d3414f3
jenyadev <88274246+jenyadev@users.noreply.github.com>
2021-08-19 11:30:17 +0300

    Update docker installation and jenkins.sh

--- docker installation and jenkins.sh
+++ docker installation and jenkins.sh
@@ -1,3 +1,11 @@
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo systemctl start docker
+# run jenkins
+mkdir -p /var/jenkins_home
+chown -R 1000:1000 /var/jenkins_home/
+docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -d --name jenkins jenkins/jenkins:lts
+
+# show endpoint
+echo 'Jenkins installed'
+echo 'You should now be able to access jenkins at: http://'$(curl -s ifconfig.co)':8080'
