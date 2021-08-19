563943640c8314f23d9005d3487c7b9352ea5187
jenyadev <88274246+jenyadev@users.noreply.github.com>
2021-08-19 11:28:42 +0300

    Update docker_instaletion.sh

--- docker_instaletion.sh
+++ docker_instaletion.sh
@@ -1,4 +1,4 @@
 curl -fsSL https://get.docker.com -o get-docker.sh
-$ sudo sh get-docker.sh
-$ sudo systemctl start docker
-$ sudo docker run hello-world
+sudo sh get-docker.sh
+sudo systemctl start docker
+sudo docker run hello-world
