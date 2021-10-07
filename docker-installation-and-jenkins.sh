a5b0908df86e7ef662023a5e25d6aefb7159287d
jenyadev <88274246+jenyadev@users.noreply.github.com>
2021-10-07 10:57:30 +0300

    Update docker-installation-and-jenkins.sh

--- docker-installation-and-jenkins.sh
+++ docker-installation-and-jenkins.sh
@@ -1,6 +1,12 @@
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-sudo systemctl start docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
+systemctl enable docker
+systemctl start docker
 
 # run jenkins
 mkdir -p /var/jenkins_home
